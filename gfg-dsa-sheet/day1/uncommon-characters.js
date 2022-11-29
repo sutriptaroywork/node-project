0bbec290eb743203cb0f0863b90ea99a64343b8c
Raramuri <sutriptaroywork@gmail.com>
2022-11-29 18:55:46 +0530

    explanation updated

--- gfg-dsa-sheet/day1/uncommon-characters.js
+++ gfg-dsa-sheet/day1/uncommon-characters.js
@@ -32,15 +32,18 @@ const uncommonCharacters = findUncommon(str1, str2);
 console.log(uncommonCharacters);
 
 
-// here we are compairing the 2nd string with the 1st string.
-// first we have used hashmap to store the characters of the 1st string.
-// then we just check is there any character in the 2nd string which is not present in the hashmap i.e. in the 1st string ?
-// if it is, we pushed that into the array and return that array
-// so here is the str1 = "geeksforgeeks" and str2 = "geeks", there will be an empty array as all the characters in the 2nd string is there in the 1st string.
-// but if we do, str2 = "geeksforgeeks" and str1 = "geeks", there will an array like this - ['f', 'o', 'r'].
-// Because teh 2nd string does have "for", which is not present into the 1st string.
+// here we are first taking two constant hashmaps of 26 items with all the value - "False".
+// as english can have maximum 26 characters, we are taking the length of 26.
+// then we are iterating two loops to get all the characters of the two strings.
+// then everytime, we are getting the distance of the character from "a" by subtracting the ASCII code of "a" from the ASCII code of that character.
+// everytime we are storing "True" to the item in hashmap with that difference.
+// at the end, we are comparing both the hashmaps by using XOR checking.
+// XOR returns "true", if both are different.
+// So if a charactar is true in one of those two hashmaps, then only it will give true.
+// if it is present or missing in both the cases i.e. same in both the cases, it returns false.
+// so trough this we can avle to acheive the different characters of the two strings.
 
 
-// so here space complexity = O(n) and time complexity = O(n).
-// as there is no nested for loop, the time complexity will be O(n).
-// as we are using an array to store our final values, it will be a space complexity of O(n).
+// so here space complexity = O(1) and time complexity = O(n + m).
+// as there is no nested for loop, the time complexity will be O(n + m) i.e. the length of two arrays.
+// as we are using two hashmaps which have a length of 26 each, the length of the hashmap is fixed. so the space complexity is constant i.e. O(1). 
