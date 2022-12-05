a43610d0dd7696d54290f94923f7cca766e09473
Raramuri <sutriptaroywork@gmail.com>
2022-12-05 14:22:59 +0530

    explanation updated and logic updated

--- gfg-dsa-sheet/day3/subarray-with-given-sum.js
+++ gfg-dsa-sheet/day3/subarray-with-given-sum.js
@@ -1,8 +1,8 @@
 function subArrayGivenSum(arr, target) {
     let start = 0;
-    let currentSum = 0;
+    let currentSum = arr[0];
 
-    for(let i = 0; i <= arr.length; i++) {
+    for(let i = 1; i <= arr.length; i++) {
         while(currentSum > target && start < i) {
             currentSum -= arr[start];
             start += 1;
@@ -20,7 +20,7 @@ function subArrayGivenSum(arr, target) {
 }
 
 const arr = [1, 2, 3, 7, 11];
-const target = 14
+const target = 12
 const subArrayWithTarget = subArrayGivenSum(arr, target);
 console.log(subArrayWithTarget);
 
@@ -36,3 +36,13 @@ console.log(subArrayWithTarget);
 // here time complexity of the solution will be - O(n) and space complexity will be - O(1).
 // as we are iterating the whole array, the time is directly dependent on the size of the array. So the time complexity will be O(n).
 // as we are storing only the starting point that doesn't dependent on the size of the array, the space complexity will be O(1).
+
+
+
+// Notes :
+//      1) here we are starting for loop from 1 because we are initially taking 0th element as current sum.
+//      2) then we are adding 1 with start as we are returning the starting item no, not index no. So 2nd index means 3rd item. As we are returning the item, we are adding 1 with the starting position.
+//      3) then we are sending ending position without adding 1, because we are checking the sum till the (i - 1)th element. If not matching then only we are adding "i"th element. So here the index is "i - 1" and item is "i - 1 + 1", that means "i".
+//      4) then we are adding "i" th item, if it is less than the array length. This we need to check as we are running the array till (array length + 1)th item.
+//      5) now why we are running array till (array length + 1)th item ? because we need to check the sum after adding the last item. Now after adding the last item, there is no checking happeing. So we are running the iteration till (array length + 1)th item.
+//      6) now why we are checking before addition ? because we are taking 0th item as current sum initially, if it not matches with the target, then only we add the next item.
